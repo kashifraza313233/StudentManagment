8b7301d9a9c0d00f4f014b021b4d6953436c7bee
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2023-01-13 00:05:52 +0500

    Initial Commit

---
+++ StudentManagmentSystem/SMS.WebApp/Controllers/LoginController.cs
@@ -0,0 +1,23 @@
+﻿using SMS.WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SMS.WebApp.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(User user)
+        {
+            return View(user);
+        }
+    }
+}
