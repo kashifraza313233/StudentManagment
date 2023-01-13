463d97299a673dbd0b12b5988f550a5519f75b4d
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2023-01-13 23:34:04 +0500

    Initial Commit

--- StudentManagmentSystem/SMS.WebApp/Controllers/LoginController.cs
+++ StudentManagmentSystem/SMS.WebApp/Controllers/LoginController.cs
@@ -7,16 +7,36 @@ using System.Web.Mvc;
 
 namespace SMS.WebApp.Controllers
 {
+    
     public class LoginController : Controller
     {
+        StudentManagmentSystemEntities db=new StudentManagmentSystemEntities();
         // GET: Login
         public ActionResult Index()
         {
             return View();
         }
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Index(User user)
         {
+            if(ModelState.IsValid)
+            {
+               using(StudentManagmentSystemEntities db=new StudentManagmentSystemEntities())
+                {
+                    var obj = db.Users.Where(x => x.User_Name.Equals(user.User_Name) && x.Password.Equals(x.Password)).FirstOrDefault();
+                    if (obj != null)
+                    {
+                        Session["UId"] = obj.UId;
+                        Session["User_Name"] = obj.User_Name;
+                        return RedirectToAction("Index", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("","UserrName && Password Incorrect");
+                    }
+                }
+            }
             return View(user);
         }
     }
