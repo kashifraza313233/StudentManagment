e7bc709956f7921cb0954702750daccf8a7690da
Kashif Raza <93009552+kashifraza313233@users.noreply.github.com>
2023-01-18 19:35:17 +0500

    Update LoginController.cs

--- StudentManagmentSystem/SMS.WebApp/Controllers/LoginController.cs
+++ StudentManagmentSystem/SMS.WebApp/Controllers/LoginController.cs
@@ -9,6 +9,7 @@ namespace SMS.WebApp.Controllers
 {
     public class LoginController : Controller
     {
+        StudentManagmentSystemEntities2 db =new StudentManagmentSystemEntities2();
         // GET: Login
         public ActionResult Index()
         {
@@ -18,6 +19,24 @@ namespace SMS.WebApp.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Index(User user)
         {
+            if(ModelState.IsValid)
+            {
+                using (StudentManagmentSystemEntities2 db = new StudentManagmentSystemEntities2())
+                {
+                    var usr = db.Users.Where(x => x.Email.Equals(x.Email) && x.Password.Equals(x.Password)).FirstOrDefault();
+                    if (usr != null)
+                    {
+                        Session["UId"] = user.UId.ToString();
+                        Session["Email"] = user.Email.ToString();
+                        return RedirectToAction("Index", "Courses");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("","Email or Password is Incorrect!");
+                    }
+                }
+            }
+           
             return View();
         }
     }
